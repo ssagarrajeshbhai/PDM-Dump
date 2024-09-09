571d9884868b66698ed0eb4cf7a6114144bcdf3b
ssagarrajeshbhai <ssagarrajeshbhai@deloitte.com>
2024-09-10 01:03:25 +0530

    creating json data for insertion

---
+++ scripts/create_json_for_seed_data.py
@@ -0,0 +1,72 @@
+from dummy_request_data import transaction_id_list, material_type_list, success_flag, error_id_list
+import random, json
+
+payload_obj = {
+    "transactionId": "123e4567e89b12d3a456426614174000",
+    "item": {
+        "basicData": {
+            "materialNumber": "",
+            "materialType": "",
+            "materialGroup": "",
+            "grossWeight": 2.3,
+            "transportationGroup": 3,
+            "netWeight": 3,
+            "crossPlantStatusCode": 12,
+            "division": 1,
+            "waferConductivityType": "sdf",
+            "waferSpecialIdCode": "12344s",
+            "waferSequenceCode": "aas2",
+            "supplierCode": "23gg"
+        },
+        "classificationData": {
+            "characteristicName": "",
+            "characteristicValue": "a",
+            "class": "",
+            "classType": 4
+        },
+        "hierarchyLinkData": {
+            "forecastClass": "",
+            "forecastDevice": "",
+            "forecastPackage": ""
+        },
+        "materialLongText": {
+            "lineNumber": 12345,
+            "applicationObject": "",
+            "textName": "",
+            "textId": 1244
+        }
+    }
+}
+
+
+def generate_sample_data():
+    final_json = []
+    # for i in range(50):
+
+    for t_id in range(10):
+        trans = {
+            "transaction_id": transaction_id_list[t_id],
+            "material_type": random.choice(material_type_list),
+            "mmid": f'mmid{t_id + 1}',
+            "incoming_payload": payload_obj,
+            "success_flag": random.choice(success_flag)
+        }
+
+        if trans["success_flag"]:
+            trans["succes_payload"] = trans["incoming_payload"]
+            trans["error_id"] = None,
+            trans["event_status_message"] = "Success"
+        else:
+            trans["succes_payload"] = None
+            trans["error_id"] = random.choice(error_id_list),
+            trans["event_status_message"] = "Failed"
+
+        final_json.append(trans)
+
+    return json.dumps(final_json, indent=3)
+
+
+j_data = generate_sample_data()
+file_name = 'data.json'
+with open(file_name, 'w') as json_writer:
+    json_writer.write(j_data)
--- scripts/dummy_request_data.py
+++ scripts/dummy_request_data.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
-csv_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\request_data.csv"
-df = pd.read_csv(csv_path)
+transaction_id_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\request_data.csv"
+material_type_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\material_type_data.csv"
+error_id_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\dummy_error_data.csv"
 
-transaction_id_list = df['transaction_id'].tolist()
-material_type = ''
+transaction_id_df = pd.read_csv(transaction_id_path)
+material_type_df = pd.read_csv(material_type_path)
+error_id_df = pd.read_csv(error_id_path)
+
+transaction_id_list = transaction_id_df['transaction_id'].tolist()
+material_type_list = material_type_df['id'].tolist()
 success_flag = ['True', 'False']
-error_id = ''
+error_id_list = error_id_df['id'].tolist()
 
 print(transaction_id_list)
+print(material_type_list)
+print(error_id_list)
