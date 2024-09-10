5aa96d1da493fe5fb31c2e62619b7351641fdf7e
ssagarrajeshbhai <ssagarrajeshbhai@deloitte.com>
2024-09-10 10:57:26 +0530

    creating jsons data for insertion in our designed db model

---
+++ scripts/create_json_seed_for_design.py
@@ -0,0 +1,34 @@
+from dummy_request_data import transaction_id_list, material_type_list, payload_id_list, success_flag, error_id_list
+import random, json
+
+op_payload_id = "b8b176c4-a0e7-4874-a32d-363f01f6c8ce"
+def generate_sample_data():
+    final_json = []
+    # for i in range(50):
+
+    for t_id in range(1000):
+        trans = {
+            "transaction_id": transaction_id_list[t_id%len(transaction_id_list)],
+            "material_type": random.choice(material_type_list),
+            "mmid": f'mmid{t_id + 1}',
+            "incoming_payload": random.choice(payload_id_list),
+            "success_flag": random.choice(success_flag)
+        }
+
+        if trans["success_flag"]:
+            trans["succes_payload"] = op_payload_id
+            trans["error_id"] = None
+            trans["event_status_message"] = "Success"
+        else:
+            trans["succes_payload"] = None
+            trans["error_id"] = f'{random.choice(error_id_list)}'
+            trans["event_status_message"] = "Failed"
+
+        final_json.append(trans)
+
+    return json.dumps(final_json, indent=3)
+
+j_data = generate_sample_data()
+file_name = f'design_json.json'
+with open(file_name, 'w') as json_writer:
+    json_writer.write(j_data)
--- scripts/dummy_request_data.py
+++ scripts/dummy_request_data.py
@@ -3,16 +3,19 @@ import pandas as pd
 transaction_id_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\request_data.csv"
 material_type_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\material_type_data.csv"
 error_id_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\dummy_error_data.csv"
+payload_id_path = r"C:\Users\ssagarrajeshbhai\PycharmProjects\PDM-Dump\src\payload_ids.csv"
 
 transaction_id_df = pd.read_csv(transaction_id_path)
 material_type_df = pd.read_csv(material_type_path)
 error_id_df = pd.read_csv(error_id_path)
+payload_id_df = pd.read_csv(payload_id_path)
 
 transaction_id_list = transaction_id_df['transaction_id'].tolist()
 material_type_list = material_type_df['id'].tolist()
 success_flag = [True, False]
 error_id_list = error_id_df['id'].tolist()
+payload_id_list = payload_id_df['id'].tolist()
 
-print(transaction_id_list)
-print(material_type_list)
-print(error_id_list)
+# print(transaction_id_list)
+# print(material_type_list)
+# print(error_id_list)
